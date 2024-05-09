fdf12625ed2b6def63b91aa0220e6db3a727facd
vritta <anuhartripathi15@gmail.com>
2024-05-09 22:11:18 +0530

    home styling

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -20,32 +20,36 @@ const Product = ({post}) => {
   }
   
   return (
-    <div className="flex flex-col items-center">
-    <div>
+    <div className="flex flex-col items-center hover:scale-110 transition 
+    duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl outline hover:shadow-[0px_0px_20px_10px_#667eea]">
+    <div className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">
       {post.title}
     </div>
     <div>
-      <p>{post.description.split(" ").slice(0,10).join(" ")+"..."}</p>
+      <p className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0,10).join(" ")+"..."}</p>
     </div>
-    <div>
-      <img src={post.image} alt="" />
+    <div className="h-[180px]">
+      <img src={post.image} alt="" className="h-full w-full"/>
     </div>
-    <div>
-      <p>{post.price}</p>
+
+    <div className="flex justify-between items-center w-full mt-5">
+      <div>
+        <p className="text-green-600 font-semibold">${post.price}</p>
+      </div>
+      <button>
+        {
+          cart.some((p) => p.id === post.id) ? (
+            <button onClick={removeFromCart}>
+              Remove Item
+            </button>
+          ) :(
+            <button onClick={addToCart}>
+              Add to Cart
+            </button>
+          )
+        }
+      </button>
     </div>
-    <button>
-      {
-        cart.some((p) => p.id === post.id) ? (
-          <button onClick={removeFromCart}>
-            Remove Item
-          </button>
-        ) :(
-          <button onClick={addToCart}>
-            Add to Cart
-          </button>
-        )
-      }
-    </button>
   </div>
 );
 };
