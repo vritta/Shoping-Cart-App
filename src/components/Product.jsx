8b2246edfb727db23b7fffec07b79823b486ad7c
vritta <anuhartripathi15@gmail.com>
2024-05-10 08:37:35 +0530

    set box size

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -20,7 +20,7 @@ const Product = ({post}) => {
   }
   
   return (
-    <div className="flex flex-col items-center hover:scale-110 transition 
+    <div className="flex flex-col items-center justify-between hover:scale-110 transition 
     duration-300 ease-in gap-3 p-4 mt-10 ml-5 rounded-xl outline hover:shadow-[0px_0px_20px_10px_#667eea]">
     <div className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">
       {post.title}
@@ -34,31 +34,29 @@ const Product = ({post}) => {
       <img src={post.image} alt="" className="h-full w-full"/>
     </div>
 
-    <div className="flex justify-between items-center w-full mt-5">
+    <div className="flex justify-between gap-12 items-center w-full mt-5">
       <div>
         <p className="text-green-600 font-semibold">${post.price}</p>
       </div>
-      <button>
-        {
-          cart.some((p) => p.id === post.id) ? (
-            <button 
-            className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
-            text-[12px] p-1 px-3 uppercase 
-            hover:bg-gray-700
-            hover:text-white transition duration-300 ease-in" onClick={removeFromCart}>
-              Remove Item
-            </button>
-          ) :(
-            <button
-            className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
-            text-[12px] p-1 px-3 uppercase 
-            hover:bg-gray-700
-            hover:text-white transition duration-300 ease-in" onClick={addToCart}>
-              Add to Cart
-            </button>
-          )
-        }
-      </button>
+      {
+        cart.some((p) => p.id === post.id) ? (
+          <button 
+          className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
+          text-[12px] p-1 px-3 uppercase 
+          hover:bg-gray-700
+          hover:text-white transition duration-300 ease-in" onClick={removeFromCart}>
+            Remove Item
+          </button>
+        ) :(
+          <button
+          className="text-gray-700 border-2 border-gray-700 rounded-full font-semibold 
+          text-[12px] p-1 px-3 uppercase 
+          hover:bg-gray-700
+          hover:text-white transition duration-300 ease-in" onClick={addToCart}>
+            Add to Cart
+          </button>
+        )
+      }
     </div>
   </div>
 );
